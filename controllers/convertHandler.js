684a3cb4ab2e310ecb68ce83143e1217f57177ee
Michael McCoy <mccoymichaeljohn@gmail.com>
2019-11-27 11:41:48 -0500

    api is done, just need to finish the tests.

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -9,23 +9,32 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let nums = input.match(/^[\d\/.]+/)
-    let divisor = nums[0].match(/\//gi)
-    
-    if (divisor !== null) {
-      let numbers = input.match(/[\d.]+/gi)
-      let divided = numbers[0]/numbers[1];
-      return divided
+    let index = input.search(/[A-Za-z]/)
+    let divisor = input.search(/\//);
+    if (divisor > 0) {
+      let num1 = parseFloat(input.slice(0, divisor));
+      let num2 = parseFloat(input.slice(divisor +1, index));
+      return num1 / num2
     }
-    
-    if (nums !== null)  return parseFloat(nums[0]);
-    return 'invalid number'
+    if (index > 0) {
+      let number = parseFloat(input.slice(0, index));
+      if (isNaN(number)) return "invalid number"
+      return number;
+    }
+    if (index === -1) {
+      let num = parseFloat(input)
+      if (isNaN(num)) return 'invalid number'
+      return num;
+    }
+    return 1;
+  
   };
   
   this.getUnit = function(input) {
     let unit = input.match(/[a-zA-Z]+/gi)
-    
-    return unit;
+    if (unit === null) return 'invalid unit';
+    if (unit[0] === 'mi' || unit[0] === 'km' || unit[0] === 'gal' || unit[0] === 'L' || unit[0] === 'lbs' || unit[0] === 'kg') return unit[0]
+    return 'invalid unit';
   };
   
   this.getReturnUnit = function(initUnit) {
@@ -35,6 +44,7 @@ function ConvertHandler() {
     if (initUnit === 'L') return 'gal'
     if (initUnit === 'lbs') return 'kg'
     if (initUnit === 'kg') return 'lbs'
+    if (initUnit === 'invalid unit') return 'invalid unit';
   };
 
   this.spellOutUnit = function(unit) {
@@ -44,6 +54,7 @@ function ConvertHandler() {
     if (unit === 'L') return 'liters'
     if (unit === 'lbs') return 'pounds'
     if (unit === 'kg') return 'kilograms'
+    if (unit === 'invalid unit') return 'invalid unit'
   };
   
   this.convert = function(initNum, initUnit) {
@@ -57,11 +68,16 @@ function ConvertHandler() {
     if (initUnit === 'L') return initNum / galToL
     if (initUnit === 'lbs') return initNum * lbsToKg
     if (initUnit === 'kg') return initNum / lbsToKg
+    if (initUnit === 'invalid unit') return initNum
   
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let string = `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`
+    if (initNum === 'invalid number' && initUnit === 'invalid unit') return 'invalid number and unit'
+    if (initNum === 'invalid number') return 'invalid number'
+    if (initUnit === 'invalid unit') return 'invalid unit'
+
+    let string = `${initNum} ${initUnit} converts to ${returnNum.toFixed(5)} ${returnUnit}`
     return string
   };
   
